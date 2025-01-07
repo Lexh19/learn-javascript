5aaf0c3ee018edf4f04a636fa63bf509c8a85afa
Lexh19 <iqbaalhzd@gmail.com>
2025-01-08 00:27:30 +0700

    day 1

---
+++ day1/index.js
@@ -0,0 +1,15 @@
+
+let nama = 'iqbaal h'// tipe string
+let usia = '22' // tipe interger number
+let tinggiBadan = 174.5 //tipe double float
+let beratBadan 
+let nikah = null
+
+if 
+
+alert(
+    
+    `nama saya  ${nama} usia saya ${usia} berat badan ${beratBadan}tinggi bandan
+     ${tinggiBadan} kapan nikah ${nikah}`,
+
+)
