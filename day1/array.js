831882f0cbeb1485babc31ada1722b73011f6e4b
Lexh19 <iqbaalhzd@gmail.com>
2025-01-08 19:13:53 +0700

    day 2 learn java script

---
+++ day1/array.js
@@ -0,0 +1,22 @@
+// //array lern
+
+// let nameTeacher = ['alex', 'altop', 'dea']
+
+// //entry data
+// nameTeacher.push('iqbaal','akmal')
+
+// //delate data firt
+// nameTeacher.shift()
+
+// //delate data firt
+// nameTeacher.pop()
+
+// alert (nameTeacher)
+
+// number 2, input data
+let nameTeacher=[]
+
+nameTeacher[0]= 'alex'
+nameTeacher[1]= 'altop'
+
+alert (nameTeacher)
